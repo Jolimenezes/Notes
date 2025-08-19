8187c3d84d58d0977ffedf22f891c28a55eefb2a
Jônatas <jolimenezes@gmail.com>
2025-08-18 22:29:36 -0300

    First Commit

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+       
+        private void Edit_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Completed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SalvarBtn_Clicked(object sender, EventArgs e)
+        {
+            string conteudo = Edit.Text;
+            File.WriteAllText(caminho, conteudo);
+        }
+
+        private void ApagarBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
